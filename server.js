f5bd6b10e404bd55bf5c38f4ce1a6ff4bf5b22e0
lau1944 <a954723219@gmail.com>
2022-10-23 14:26:51 -0400

    feat: rm static view

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', express.static(__dirname + '/src/views'));
+//app.use('/', express.static(__dirname + '/src/views'));
 
 const uploadForm = upload.fields([{ name: 'photo', maxCount: 1 }]);
 // post photo
